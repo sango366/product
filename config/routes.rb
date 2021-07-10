9088680c46002889c8b6c56627f04a1f7932d33f
深澤瑚大 <sango@fukazawanoMacBook-Air.local>
2021-07-10 14:39:25 +0900

    comolite follow function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users,only: [:show,:edit,:update,:index] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+  root 'homes#top'
+  get 'homes/about'
 end
